052a2861a147b387f0b50ad75e9967f1f18add18
Vaibhav Patidar <vaibhavpatidar22012005@gmail.com>
2026-09-20 06:23:18 +0530

    concurrency

--- server/src/worker/loadTest.ts
+++ server/src/worker/loadTest.ts
@@ -1,52 +1,48 @@
+import { error } from "node:console";
+
 const url = "http://localhost:3000/test";
 
 async function loadtest() {
 
     const startTime: number = Date.now();
-const actualReqSend = 100;
+    const actualReqSend = 100;
 
-    let totalReqExecuted: number = 0;
     let successfulRequests: number = 0;
     let failedReq: number = 0;
-for(let i=1;i<=actualReqSend;i++){
 
+    const requests:Promise<Response>[] = [];
 
-    try {
-        const response = await fetch(url);
+    for (let i = 1; i <= actualReqSend; i++) {
+        if(i===50){
+            requests.push(Promise.reject(new Error("rejected requested 50 manually")));
+            continue;
+        }
+        const promise = fetch(url);
+        console.log("request",i,promise);
+        requests.push(promise);
+    }
 
-        totalReqExecuted++;
-
-        console.log("Status:", response.status);
-
-        if (response.ok) {
-            console.log("Request success");
+    const responses = await Promise.allSettled(requests);
+    console.log(responses[0]);
+    for (let i = 0; i < responses.length; i++) {
+        if (responses[i]?.status==="fulfilled") {
             successfulRequests++;
         }
         else {
-            console.log("Request fail");
             failedReq++;
         }
     }
-    catch (error) {
-        console.log("Error:", error);
-        totalReqExecuted++;
-        failedReq++;
-    }
-}
 
     const finalTime: number = Date.now();
-    
-
     const totalTime: number = finalTime - startTime;
+
     const requestsPerSecond = actualReqSend / (totalTime / 1000);
 
-    console.log("Total Actual Requests:", actualReqSend)
-    console.log("Total Requests Executed:", totalReqExecuted);
+    console.log("Total Requests:", actualReqSend);
     console.log("Successful:", successfulRequests);
     console.log("Failed:", failedReq);
     console.log("Total Time:", totalTime, "ms");
     console.log("Requests Per Second:", requestsPerSecond);
-
 }
 
 loadtest();
